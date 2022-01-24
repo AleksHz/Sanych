8d3b0651817ebf818a71cf349ba0c2f506bcd528
Sanych <leshka.gazuk@mail.ru>
2022-01-24 21:43:23 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,108 @@
+"use strict";//strogo novye znacenia 
+
+
+/////////////// 1 urok ///////////////
+/*let number = 5; //mozno menat 
+const leftBorderWidth = 1; // nelza menat
+
+
+number = 10; 
+console.log(number);
+
+var name = 'Aleks';//staroe*/
+
+/////////////// 2 urok ///////////////
+
+/*let number = 5; //lubye cisla (ne scitaja ogromnych)
+
+const persone = "Aleks"; 
+
+const bool = true; //false
+
+const obj = {
+    name: "Aleks",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name); //luchshe cerez tochku
+//console.log(obj["name"]); 
+
+let arr = ['plum.png', 2, {}, [], 'orange.jpg'];//cto ugodno
+console.log(arr[2]);//numeracya s nula */
+
+/////////////// 3 urok ///////////////
+
+/*alert('hello!'); //vyzov
+
+const result = confirm('How are you?'); //vyzov s variantami otweta
+
+const answer = prompt ("How are you?", ""); //vyzov s polem vvoda
+
+const answers = [];
+answers[0] = prompt('whats your name?', '');
+answers[1] = prompt('whats your name2?', '');
+answers[2] = prompt('whats your name3?', '');
+answers[3] = prompt('whats your name4?', '');
+document.write(answers);
+*/
+
+/////////////// 4 urok ///////////////
+
+/*
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`); //interpolacya
+
+const user = 'Aleks';
+alert(`Hi, ${user}!`);
+*/
+
+/////////////// 5 urok ///////////////
+
+/*
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+++incr;
+--decr;
+
+console.log(5%2); //pokazywaet ostatok
+
+console.log(2*2 == 8); //srawnenie
+
+console.log(2*2 === 8);//strogoe srawnenie  
+
+&& // i (tolko jesli oba prawdiwy)
+|| // ili
+
+const isChecked = true,
+        isClose = true;
+console.log(isChecked && isClose);
+
+*/
+
+/////////////// 6 urok ///////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
